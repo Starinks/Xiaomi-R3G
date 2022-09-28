8bc500fad2b9f08976bcd07bf80747e1ba6ddd6a
Mahiru <41849505+Starinks@users.noreply.github.com>
2022-09-28 13:14:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,8 +12,3 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-#启用软件
-cat >> .config <<EOF
-CONFIG_PACKAGE_luci-app-argon-config=y
-EOF
